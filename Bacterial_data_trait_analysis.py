1961fa6d06ff92320ed5d247fadf13b044d22ea7
GuiraudTresor <80392590+GuiraudTresor@users.noreply.github.com>
2023-01-20 15:16:13 +0100

    import dependencies and control custom funcs

--- Bacterial_data_trait_analysis.py
+++ Bacterial_data_trait_analysis.py
@@ -1,3 +1,41 @@
+%matplotlib inline
+
+from skbio.tree import TreeNode
+from ggmap.snippets import *
+from ggmap.settings import *
+from ggmap.analyses import *
+import pandas as pd
+import os
+import tempfile
+from tqdm import tqdm
+
+import pylab as plt
+from matplotlib_venn import venn2, venn2_circles, venn3, venn3_circles
+
+def _cols_check(table: pd.DataFrame, cols: [str]):
+    assert all([c in table.columns for c in cols]), \
+        "some columns in 'cols' are not in your table!"
+
+        
+def find_multiword_rows(table: pd.DataFrame, cols: [str]) -> pd.DataFrame:
+    _cols_check(table, cols)
+
+    _filter = None
+    for col in cols:
+        err = table[col].apply(lambda x: len(x.split()) > 1)
+        if _filter is None:
+            _filter = err
+        else:
+            _filter |= err
+
+    return table[_filter]
+
+def check_singleword(entries):
+    for w in entries:
+        assert len(w.strip().split(" ")) == 1, "'%s' is not a single word" % w
+        
+#find_padded_rows(tr_tresor, cols=['Genus'])
+
 # Read John Guittar trait's table
 tr_guitar = pd.read_csv('curated_trait_data.csv', index_col=0)
 tr_guitar.head()
